38d47b70755584e87544fff991ed496dbeac5bb5
Jeffrey He <jeffrey.he3@student.tdsb.on.ca>
2025-04-08 13:20:38 -0400

    Changed imports

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,11 @@
 function Footer() {
 
     return (
-        <></>
+        <div>
+            <a href="https://github.com/JeffreyHe3/Assignment-5" target="_blank">GitHub</a>
+            <p>Copyright &copy; 2025; Designed by Jeffrey He</p>
+        </div>
     )
-  }
-  
-  export default Footer
+}
+
+export default Footer
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,12 @@
 function Header() {
 
     return (
-        <></>
+        <div>
+            <h id="title">Jeffrey's Movies</h>
+            <button>Log In</button>
+            <button>Create Account</button>
+        </div>
     )
-  }
-  
-  export default Header
+}
+
+export default Header
