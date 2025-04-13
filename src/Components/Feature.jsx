d000414133252aab06d908c37c72996c02f58ae1
JeffreyHe3 <jeffrey.he3@student.tdsb.on.ca>
2025-04-13 19:23:06 -0400

    Changed files

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -1,28 +1,33 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import './style.css'
-import { useLocation, useNavigate } from "react-router-dom";
+import './style.css';
 
 function Feature() {
-    const [movies, setMovies] = useState([]);
-    const navigate = useNavigate();
+    const [movie, setMovie] = useState([]);
 
     useEffect(() => {
-        async function getData() { };
+        async function getData() {
+            try {
+                setMovie((axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_TMDB_KEY}`)).data.results);
+            } catch (e) {
+                console.log("error");
+            }
+
+        };
 
         getData();
     }, []);
 
     return (
         <div>
-            {movies && movies.map(movies => (
-                <div key={movies.id}>
-                    <img src={movies.image} alt={`${movies.id}`} />
-                    <h1>{movies.id}</h1>
+            {movie && movie.map(movie => (
+                <div>
+                    <h1>{`${movie.title}`}</h1>
+                    <img className="movie-poster" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.id}`} />
                 </div>
             ))}
         </div>
-    )
+    );
 }
 
-export default Feature
+export default Feature;
